ea2f54f2d1d3e4afe1b99d4358f6d207fed60a68
James Stanley <james@incoherency.co.uk>
2018-10-12 17:19:55 +0000

    Add option to encode using only non-blunders, as scored by p4wn

--- js/chess-steg.js
+++ js/chess-steg.js
@@ -1,4 +1,4 @@
-function chess_steg(data) {
+function chess_steg(data, use_engine) {
     let numin = bigInt(1); // start at 1 so that leading zero bytes are not lost
 
     data = encode_utf8(data);
@@ -11,7 +11,7 @@ function chess_steg(data) {
 
     let game = new Chess();
     while (numin.compare(0) == 1) { // while numin > 0
-        let moves = legal_moves(game);
+        let moves = use_engine ? reasonable_moves(game) : legal_moves(game);
 
         let pickmove = numin.mod(moves.length);
         numin = numin.subtract(pickmove).divide(moves.length);
@@ -29,7 +29,7 @@ function chess_steg(data) {
     return pgn;
 }
 
-function chess_unsteg(pgn) {
+function chess_unsteg(pgn, use_engine) {
     let numdata = bigInt(0);
 
     let game = new Chess();
@@ -42,7 +42,7 @@ function chess_unsteg(pgn) {
         }
         pickedmove = pickedmove.san;
 
-        let moves = legal_moves(game);
+        let moves = use_engine ? reasonable_moves(game) : legal_moves(game);
         let idx;
         for (let i = 0; i < moves.length; i++) {
             if (moves[i] == pickedmove) {
@@ -75,6 +75,41 @@ function legal_moves(game) {
     }
 
     return moves;
+}
+
+function reasonable_moves(game) {
+    let moves = legal_moves(game);
+
+    // this makes p4wn play deterministically
+    P4_DEBUG = 1;
+
+    // get p4wn to evaluate each move
+    for (var i = 0; i < moves.length; i++) {
+        game.move(moves[i]);
+
+        let p4state = p4_fen2state(game.fen());
+        let p4move = p4state.findmove(2);
+        moves[i] = [moves[i], -p4move[2]];
+
+        game.undo();
+    }
+
+    moves.sort(function(a,b) {
+        return b[1] - a[1];
+    });
+
+    let reasonable_moves = [moves[0][0]];
+
+    // keep moves that are not too much worse than the best move (20 points = 1 pawn)
+    for (var i = 1; i < moves.length; i++) {
+        if (moves[0][1] - moves[i][1] > 40) {
+            break;
+        }
+
+        reasonable_moves.push(moves[i][0]);
+    }
+
+    return reasonable_moves;
 }
 
 function encode_utf8(s) {
